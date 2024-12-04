1b519bd39bb0ca43de73b92d7d3e31027078daf6
dreamlilyth <dreamlilyth@gmail.com>
2024-12-03 23:41:42 -0300

    ahora si

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
+#! exit on error
 set -o errexit
+
 
 pip install -r requeriments.txt
 
-pyhton manage.py collectstatic --no-input
-pyhton manage.py migrate
+python manage.py collectstatic --no-input   
+python manage.py migrate
